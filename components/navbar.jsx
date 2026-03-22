c470c41b486ac5c5894e4bf1efc73a27c666c254
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-22 09:41:02 +0000

    Add nav links to li

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,20 +1,36 @@
 import { Link } from "react-router";
 import styles from "../styles/navbar-style.module.css";
-import { useState } from "react";
 
 function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-
-    return(
-        <nav className={styles.nav}>
-            <Link to="/homepage"> <span>//</span> Hemant Codes</Link>
-            <ul>
-                <li>POSTS</li>
-                <li>PROJECTS</li>
-                <li>GITHUB</li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className={styles.nav}>
+      <Link to="/homepage">
+        {" "}
+        <span>//</span> Hemant Codes
+      </Link>
+      <ul>
+        <li>
+          <a href="#" rel="noopener noreferrer">
+            POSTS
+          </a>
+        </li>
+        <li>
+          <a href="#" rel="noopener noreferrer">
+            X
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/HemantDahiyaCodes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GITHUB
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export {Navbar};
+export { Navbar };
