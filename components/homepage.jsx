b8f33aa4f5084fa2576f624fe5ae41164fa1b573
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2026-03-22 06:46:23 +0000

    Fix axios request to the backend

--- components/homepage.jsx
+++ components/homepage.jsx
@@ -1,41 +1,32 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
-import "../styles/homepage-style.css";
+import styles from "../styles/homepage-style.module.css";
+import { Navbar } from "./navbar";
+
 
 function HomePage() {
   const [posts, setPosts] = useState([]);
-  const [user, setUser] = useState("");
 
   useEffect(() => {
     async function getPosts() {
       const token = localStorage.getItem("token");
-      const response = await axios.get("http://localhost:8000/users/posts", {
+      const response = await axios.get("http://localhost:8000/posts", {
         headers: {
           Authorization: token,
           "Content-Type": "application/json",
         },
       });
+      console.log(response.data);
       setPosts(response.data.posts);
-      setUser(response.data.userInReq.username);
     }
     getPosts();
   }, []);
   return (
     <>
-      <div className="main-container">
-      <h1 className="user-title">Welcome {user}</h1>
-      <div className="posts-container">
-        {posts.map((post) => {
-          return (
-              <div className="post-card">
-                <h2 className="post-title">{post.title}</h2>
-                <Link to={`/posts/${post.id}`}>View</Link>
-              </div>
-          );
-        })}
-        </div>
-      </div>
+    <div className={styles.body}>
+      <Navbar />
+    </div>
     </>
   );
 }
